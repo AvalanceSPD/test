2710d3e4cdf97a3afa2b0c8fa45529fe18fed6e8
CroissantKnight <teekayu.k123@gmail.com>
2025-01-08 13:31:14 +0700

    initial commit branch pang

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('กรุณากำหนดค่า SUPABASE_URL และ SUPABASE_ANON_KEY ใน .env file')
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
