831f691f5dacb018ea1280571fcdc74863748cc4
CroissantKnight <teekayu.k123@gmail.com>
2025-01-22 08:43:57 +0700

    start modify lesson page here

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import { LessonView } from './components/Course/LessonView';
-import { CreateLesson } from './components/Course/CreateLesson';
-import { StudentProfile, TeacherProfile, Home_1 } from './pages/index';
-import { LessonList } from './components/Course/LessonList';
-import { GuestHome } from './pages/GuestHome';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import { LessonView } from "./components/Course/LessonView";
+import { CreateLesson } from "./components/Course/CreateLesson";
+import { StudentProfile, TeacherProfile, Home_1 } from "./pages/index";
+import { LessonList } from "./components/Course/LessonList";
+import { GuestHome } from "./pages/GuestHome";
 import TestPages from "./test/TestPages";
+import TestPages1 from "./test/TestPage1";
 
 export const AppRoutes = () => {
   return (
@@ -24,6 +25,7 @@ export const AppRoutes = () => {
         <Route path="/guest-home" element={<GuestHome />} />
         <Route path="/home_1" element={<Home_1 />} />
         <Route path="/test" element={<TestPages />} />
+        <Route path="/test1" element={<TestPages1 />} />
       </Routes>
     </Layout>
   );
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,2 +1,3 @@
 export { default as TestPages } from './TestPages';
 export { default as TestContent} from './TestCreateLesson';
+export { default as TestPages1} from './TestPage1';
